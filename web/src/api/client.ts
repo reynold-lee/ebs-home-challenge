6f3d2bee48b102bf5c11eaa8eb20d34e1981e347
Reynold Lee <reynold.lee720@gmail.com>
2023-09-07 23:05:17 -0400

    API url

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Item, ItemCreation } from "./types";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 apiClient.interceptors.response.use((res) => {
